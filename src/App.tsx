a83a6a6f595c77c8f3f3fa342d7f2e908b693297
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-09-04 19:42:01 +0200

    Added a background image for the site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import { Routes, Route } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 import Layout from 'components/Layout';
 
 import HomePage from 'pages/HomePage';
 
+import background from 'images/app/bg.png';
+
+const Wrapper = styled.div`
+  display: flex;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-image: url(${background});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  overflow-y: auto;
+`;
+
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Layout>
-            <HomePage />
-          </Layout>
-        }
-      />
-    </Routes>
+    <Wrapper>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomePage />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Wrapper>
   );
 };
 
