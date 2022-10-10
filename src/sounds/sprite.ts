59d8c2688f7b85724ce0e7b10f294903c7a3568a
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-10-10 21:54:35 +0200

    Added an audio sprite. Corrected work with sounds by adding sounds to the main buttons. Added the ability to turn off the sound.

---
+++ src/sounds/sprite.ts
@@ -0,0 +1,9 @@
+const sprite: any = {
+  add_coins: [0, 2076.9387755102043],
+  counter_click: [4000, 91.33786848072577],
+  level_up: [6000, 2295.759637188208],
+  mouse_click: [10000, 497.392290249433],
+  sound_victory: [12000, 1767.052154195012],
+};
+
+export default sprite;
